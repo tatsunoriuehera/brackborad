7dcee803268c3184ebe55f6d02f35185566008b2
tatsunoriuehara <uehara.t29@gmail.com>
2019-07-25 16:29:37 +0900

    first commit

---
+++ get_post.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET["comment"])){
+$comment = $_GET["comment"];
+echo $comment;
+}
+?>
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+<meta charset = "UFT-8">
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<h1>フォームデータの送信</h1>
+<form action = "get_post.php" method = "get">
+<input type = "text" name ="comment"/><br/>
+<input type ="submit" value ="送信"/>
+</form>
+</body>
+</html>
